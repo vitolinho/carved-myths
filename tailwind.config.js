1921105478b2d422e87aae13f1385b23a2c65fbb
vitolinho <93949338+vitolinho@users.noreply.github.com>
2023-11-19 15:46:16 +0100

    🎉 beginning of the project

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontSize: {
+      base: "1rem",
+      lg: "1.25rem",
+      xl: "1.5rem",
+      "2xl": "1.75rem",
+      "3xl": "2rem",
+      "4xl": "2.25rem",
+      "5xl": "8.75rem",
+      "6xl": "12.5rem"
+    },
+    extend: {
+      colors: {
+        "white": "#FFFFFF",
+        "silver": "#EAE6E6",
+        "carbone": "#343434",
+        "black": "#131313",
+        "egg-shell": "#F8F8F8"
+      }
+    },
+  },
+  plugins: [],
+}
+
