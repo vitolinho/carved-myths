f7063ab95467f2f0689bb92a6e3e62906e1d9a03
vitolinho <93949338+vitolinho@users.noreply.github.com>
2023-11-19 17:32:59 +0100

    ✨ added the second section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Navbar from "./components/navbar/navbar"
 
 import img1 from "/pictures/1.png"
 import img2 from "/pictures/2.png"
+import img3 from "/pictures/3.png"
 
 function App() {
   const [isDesktop, setIsDesktop] = useState(false)
@@ -18,7 +19,7 @@ function App() {
   }, [])
   return (
     <>
-      <ParallaxBanner className="h-screen relative">
+      <ParallaxBanner className="h-screen relative z-0">
         <ParallaxBannerLayer image={isDesktop ? img1 : img2} speed={-10} className="z-0" />
       </ParallaxBanner>
       <Navbar/>
@@ -27,7 +28,17 @@ function App() {
           carved<br></br>myths
         </p>
       </div>
-      <div className="h-screen bg-blue-200"></div>
+      <div className="bg-egg-shell relative w-full mt-[-4rem] flex flex-col justify-center items-center gap-y-[3.75rem] px-5 py-20 rounded-t-3xl mb-[12.5rem] lg:mb-[15rem] lg:px-[4.5rem] lg:gap-y-20">
+        <p className="body-lg text-carbone">welcome to</p>
+        <div className="w-full flex flex-col justify-center items-center gap-y-10 lg:gap-y-20">
+          <div className="w-full flex flex-col justify-center items-center gap-y-10 lg:flex-row lg:justify-between">
+            <p className="header-3 text-black lg:header-2">Carved</p>
+            <p className="body-md text-carbone w-[18.75rem] text-center lg:text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis</p>
+          </div>
+          <img src={img3} alt="3rd picture" className="w-full"/>
+        </div>
+      </div>
+      <div className="h-screen bg-purple-500"></div>
     </>
   )
 }
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <div className="w-full fixed top-0 flex items-center px-5 py-10 mix-blend-difference lg:px-[4.5rem]">
+    <div className="z-40 w-full fixed top-0 flex items-center px-5 py-10 mix-blend-difference lg:px-[4.5rem]">
       <p className="button text-white mix-blend-difference">carved myths</p>
     </div>
   )
